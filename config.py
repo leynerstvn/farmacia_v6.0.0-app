471d514b4065f1fde9ecfc4177b56968154cfd40
leynerstvn <stevenleyner@hotmail.com>
2026-06-28 00:23:04 -0500

    fix: use SQLite as default DB (compatible with PythonAnywhere free tier)

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'farmacia-secret-key-2024'
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://root:@localhost/farmacia_db'
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'farmacia.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
